1194e5ef4634e68601146fe92c62a5c609d47ad1
anonymus1234-cloud <84515544+anonymus1234-cloud@users.noreply.github.com>
2023-12-02 00:24:09 -0500

    Add files via upload

---
+++ train.js
@@ -0,0 +1,29 @@
+document.write("<h3>By using object literal</h3>")
+var emp1 = {
+    name:"Shayam Kumar",
+    salary:"1,000,000",
+    id:"300"
+}
+document.write("<br>Employee id is no "+emp1.id)
+document.write("<br>Employee name is "+emp1.name)
+document.write("<br>Employee salary is "+emp1.salary)
+
+document.write("<h3>by creating instance of a object directly</h3>")
+var emp2 = {
+    name:"ravi malik",
+    salary:"30,000",
+    id:"101"
+}
+document.write("<br>Employee id is no "+emp2.id)
+document.write("<br>Employee name is "+emp2.name)
+document.write("<br>Employee salary is "+emp2.salary)
+
+document.write("<h3>By using object literal</h3>")
+var emp3 = {
+    name:"vimal jaismal",
+    salary:"30,000",
+    id:"123"
+}
+document.write("<br>Employee id is no "+emp3.id)
+document.write("<br>Employee name is "+emp3.name)
+document.write("<br>Employee salary is "+emp3.salary)
